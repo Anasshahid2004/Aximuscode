c26261178ecaf76bc7902fd6a93b88aa9a116b67
Anas Shahid <anas.shahid.990@gmail.com>
2024-11-18 23:25:17 +0500

    aximuscode official company

---
+++ megakit-bootstrap-main/gulpfile.js
@@ -0,0 +1,178 @@
+"use strict";
+
+const sass = require("gulp-sass")(require("sass"));
+const gulp = require("gulp");
+const sourcemaps = require("gulp-sourcemaps");
+const fileinclude = require("gulp-file-include");
+const autoprefixer = require("gulp-autoprefixer");
+const bs = require("browser-sync").create();
+const rimraf = require("rimraf");
+const comments = require("gulp-header-comment");
+
+var path = {
+  src: {
+    html: "source/*.html",
+    others: "source/*.+(php|ico|png)",
+    htminc: "source/partials/**/*.htm",
+    incdir: "source/partials/",
+    plugins: "source/plugins/**/*.*",
+    js: "source/js/*.js",
+    scss: "source/scss/**/*.scss",
+    images: "source/images/**/*.+(png|jpg|gif|svg)",
+  },
+  build: {
+    dirBuild: "public/", // Output directory for the build
+    dirDev: "public/",   // Development directory
+  },
+};
+
+// HTML Task
+gulp.task("html:build", function () {
+  return gulp
+    .src(path.src.html)
+    .pipe(
+      fileinclude({
+        basepath: path.src.incdir,
+      })
+    )
+    .pipe(
+      comments(`
+    WEBSITE: https://themefisher.com
+    TWITTER: https://twitter.com/themefisher
+    FACEBOOK: https://www.facebook.com/themefisher
+    GITHUB: https://github.com/themefisher/
+    `)
+    )
+    .pipe(gulp.dest(path.build.dirDev))
+    .pipe(
+      bs.reload({
+        stream: true,
+      })
+    );
+});
+
+// SCSS Task
+gulp.task("scss:build", function () {
+  return gulp
+    .src(path.src.scss)
+    .pipe(sourcemaps.init())
+    .pipe(
+      sass({
+        outputStyle: "expanded",
+      }).on("error", sass.logError)
+    )
+    .pipe(autoprefixer())
+    .pipe(sourcemaps.write("/"))
+    .pipe(
+      comments(`
+    WEBSITE: https://themefisher.com
+    TWITTER: https://twitter.com/themefisher
+    FACEBOOK: https://www.facebook.com/themefisher
+    GITHUB: https://github.com/themefisher/
+    `)
+    )
+    .pipe(gulp.dest(path.build.dirDev + "css/"))
+    .pipe(
+      bs.reload({
+        stream: true,
+      })
+    );
+});
+
+// Javascript Task
+gulp.task("js:build", function () {
+  return gulp
+    .src(path.src.js)
+    .pipe(
+      comments(`
+  WEBSITE: https://themefisher.com
+  TWITTER: https://twitter.com/themefisher
+  FACEBOOK: https://www.facebook.com/themefisher
+  GITHUB: https://github.com/themefisher/
+  `)
+    )
+    .pipe(gulp.dest(path.build.dirDev + "js/"))
+    .pipe(
+      bs.reload({
+        stream: true,
+      })
+    );
+});
+
+// Images Task
+gulp.task("images:build", function () {
+  return gulp
+    .src(path.src.images)
+    .pipe(gulp.dest(path.build.dirDev + "images/"))
+    .pipe(
+      bs.reload({
+        stream: true,
+      })
+    );
+});
+
+// Plugins Task
+gulp.task("plugins:build", function () {
+  return gulp
+    .src(path.src.plugins)
+    .pipe(gulp.dest(path.build.dirDev + "plugins/"))
+    .pipe(
+      bs.reload({
+        stream: true,
+      })
+    );
+});
+
+// Other Files Task
+gulp.task("others:build", function () {
+  return gulp.src(path.src.others).pipe(gulp.dest(path.build.dirDev));
+});
+
+// Clean Task
+gulp.task("clean", function (cb) {
+  rimraf("./public", cb); // Cleans the 'public/' directory
+});
+
+// Watch Task
+gulp.task("watch:build", function () {
+  gulp.watch(path.src.html, gulp.series("html:build"));
+  gulp.watch(path.src.htminc, gulp.series("html:build"));
+  gulp.watch(path.src.scss, gulp.series("scss:build"));
+  gulp.watch(path.src.js, gulp.series("js:build"));
+  gulp.watch(path.src.images, gulp.series("images:build"));
+  gulp.watch(path.src.plugins, gulp.series("plugins:build"));
+});
+
+// Dev Task
+gulp.task(
+  "default",
+  gulp.series(
+    "clean",
+    "html:build",
+    "js:build",
+    "scss:build",
+    "images:build",
+    "plugins:build",
+    "others:build",
+    gulp.parallel("watch:build", function () {
+      bs.init({
+        server: {
+          baseDir: path.build.dirDev, // Serve files from 'public/'
+        },
+      });
+    })
+  )
+);
+
+// Build Task
+gulp.task(
+  "build",
+  gulp.series(
+    "html:build",
+    "js:build",
+    "scss:build",
+    "images:build",
+    "plugins:build",
+    "others:build"
+  )
+);
